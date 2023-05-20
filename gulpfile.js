c5df559db1176cd86ad2a15c13bbf9232b3114f8
alexxxey2308 <alexeybissenek@yandex.ru>
2023-05-19 20:20:09 -0700

    fixed errors

--- gulpfile.js
+++ gulpfile.js
@@ -126,7 +126,7 @@ const reload = (done) => {
 const watcher = () => {
   gulp.watch('source/less/**/*.less', gulp.series(styles));
   gulp.watch('source/js/script.js', gulp.series(script));
-  gulp.watch('source/*.html').on('change', browser.reload);
+  gulp.watch('source/*.html', gulp.series(html, reload));
 }
 
 export const build = gulp.series(
